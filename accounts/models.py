440b554e7c365972b8cc18a8cd8ac06993bf5462
akhilkvu6 <akhilkvu6@gmail.com>
2026-08-24 11:40:37 +0530

    feat: add student profile

--- accounts/models.py
+++ accounts/models.py
@@ -42,3 +42,18 @@ class TeacherProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class StudentProfile(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='student_profile'
+    )
+
+    roll_number = models.CharField(max_length=50)
+    department = models.CharField(max_length=100)
+    semester = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.username
